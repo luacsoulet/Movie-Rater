fbaf734e35502d1f945a2a53f082a1e7ec04b64e
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-07-25 02:20:25 +0200

    Page.jsx Card import & update
    Importation du composant Card en lui passant en props les données des films trier soit par date de sortie soit par note.
    En mettant en forme les affiches sous forme de rangées de 5 affiches.

--- movie-rater/app/page.jsx
+++ movie-rater/app/page.jsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import Card from "./components/ui/card";
+import MoviesData from '@/public/data.json'
+
+function Home() {
+
+  const data = MoviesData.films;
+
+  const recentMovies = data.sort((a,b) => b.year - a.year).slice(0,5);
+
+  const bestRating = data.sort((a,b) => b.rating - a.rating).slice(0,5);
+  
   return (
-    <main></main>
+    <main className="flex flex-col justify-center items-center text-white gap-11">
+      <section className="mt-16">
+        <h1 className="text-lg font-extrabold">Latest Movies Added:</h1>
+        <div className="flex">
+          {recentMovies.map((el) => (
+            <Card 
+              key={el.id}
+              title={el.title}
+              year={el.year}
+              rating={el.rating}
+              poster={el.poster}
+            />
+          ))}
+        </div>
+      </section>
+      <section>
+        <h1 className="text-lg font-extrabold">Best ratings:</h1>
+        <div className="flex">
+        {bestRating.map((el) => (
+            <Card 
+              key={el.id}
+              title={el.title}
+              year={el.year}
+              rating={el.rating}
+              poster={el.poster}
+            />
+          ))}
+        </div>
+      </section>
+    </main>
   );
 }
+
+export default Home
