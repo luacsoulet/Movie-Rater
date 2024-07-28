87987d7315fa0ddffdefb1bc19d40eb786b2572d
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-07-28 18:38:47 +0200

    Home page update
    Ajout de certaines classes Tailwind pour une meilleure structure visuelle de la page home et de ces composants. Changement du props donner au composant Card de rating à averageRating.

--- movie-rater/app/page.jsx
+++ movie-rater/app/page.jsx
@@ -7,33 +7,33 @@ function Home() {
 
   const recentMovies = data.sort((a,b) => b.year - a.year).slice(0,5);
 
-  const bestRating = data.sort((a,b) => b.rating - a.rating).slice(0,5);
+  const bestRating = data.sort((a,b) => b.averageRating - a.averageRating).slice(0,5);
   
   return (
-    <main className="flex flex-col justify-center items-center text-white gap-11">
-      <section className="mt-16">
-        <h1 className="text-lg font-extrabold">Latest Movies Added:</h1>
-        <div className="flex">
+    <main className="flex flex-col justify-center items-center text-white gap-10">
+      <section className="flex flex-col mt-10 gap-5">
+        <h1 className="text-center text-xl font-medium">Latest Movies Added:</h1>
+        <div className="flex gap-10">
           {recentMovies.map((el) => (
             <Card 
               key={el.id}
               title={el.title}
               year={el.year}
-              rating={el.rating}
+              averageRating={el.averageRating}
               poster={el.poster}
             />
           ))}
         </div>
       </section>
-      <section>
-        <h1 className="text-lg font-extrabold">Best ratings:</h1>
-        <div className="flex">
+      <section className="flex flex-col gap-5">
+        <h1 className="text-center text-xl font-medium">Best ratings:</h1>
+        <div className="flex gap-10">
         {bestRating.map((el) => (
             <Card 
               key={el.id}
               title={el.title}
               year={el.year}
-              rating={el.rating}
+              averageRating={el.averageRating}
               poster={el.poster}
             />
           ))}
