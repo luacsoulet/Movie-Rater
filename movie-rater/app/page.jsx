17a819089c9e7854e6bd413cdd0e9cbf329a31ce
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2024-07-30 01:16:54 +0200

    Home page import modal & code update
    Importation de composant InfoModal ainsi que l'importation de la librairie react-modal. Cela servira à afficher les informations manquantes des films en récupérer leurs donner via la fonction callback handleMovieSelect. pour le redonner au composant InfoModal. Ajout d'un id a la balise main de la page pour y rattacher la modale. Passage de la page en server-side en client-side via "use client" pour permettre l'interaction avec les boutons et l'apparition de la modale. Importation de useState pour maintenir les informations ainsi que de fonctions callback pour définir les useState. Et ajout des commentaires.

--- movie-rater/app/page.jsx
+++ movie-rater/app/page.jsx
@@ -1,26 +1,64 @@
+'use client'
+import Modal from 'react-modal';
+import { useState } from "react";
+import InfoModal from "./components/ui/modal";
 import Card from "./components/ui/card";
 import MoviesData from '@/public/data.json'
 
-function Home() {
+// Définir a quel élément parent la modal sera rattacher
+Modal.setAppElement('#app');
 
+function Home() {
+  // UseState pour l'ouverture / fermeture de la modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // UseState pour la récupération des datas du film selectionné
+  const [isMovieData, setIsMovieData] = useState([]);
+  
+  // Sélection des données des films dans le fichier .json de test
   const data = MoviesData.films;
 
+  // Sélection des films par année au plus récent
   const recentMovies = data.sort((a,b) => b.year - a.year).slice(0,5);
 
+  // Sélection des films par note moyenne aux plus hautes
   const bestRating = data.sort((a,b) => b.averageRating - a.averageRating).slice(0,5);
+
+
+  // Récupération de la props du component child Card pour savoir quel film à été choisi
+  const handleMovieSelect = (id) => {
+
+    // Cherche les données suivant l'affiche où l'on a cliqué pour avoir plus d'infos.
+    const movieData = data.find(el => el.id === id);
+
+    // Mettre les données du film dans le useState 
+    setIsMovieData(movieData);
+
+    // Définir l'ouverture de la modal sur true pour l'afficher
+    setIsModalOpen(true);
+  };
+
+  // callback function pour définir la fermeture de la modal lors d'un clique en dehors de la modal
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setIsMovieData(null);
+  };
   
   return (
-    <main className="flex flex-col justify-center items-center text-white gap-10">
+    <main className="flex flex-col justify-center items-center text-white gap-10" id='app'>
       <section className="flex flex-col mt-10 gap-5">
         <h1 className="text-center text-xl font-medium">Latest Movies Added:</h1>
         <div className="flex gap-10">
+          {/* Génération des Card pour les films les plus récents */}
           {recentMovies.map((el) => (
             <Card 
               key={el.id}
+              id={el.id}
               title={el.title}
               year={el.year}
               averageRating={el.averageRating}
               poster={el.poster}
+              onMovieSelect={handleMovieSelect}
             />
           ))}
         </div>
@@ -28,17 +66,31 @@ function Home() {
       <section className="flex flex-col gap-5">
         <h1 className="text-center text-xl font-medium">Best ratings:</h1>
         <div className="flex gap-10">
+        {/* Génération des Card pour les films les mieux notés */}
         {bestRating.map((el) => (
             <Card 
               key={el.id}
+              id={el.id}
               title={el.title}
               year={el.year}
               averageRating={el.averageRating}
               poster={el.poster}
+              // onMovieSelect vas servir à faire passer la propriété true de isModalOpen si le bouton plus de la card est cliqué
+              onMovieSelect={handleMovieSelect}
             />
           ))}
         </div>
       </section>
+      {/* si le useState isModalOpen = true alors la modal s'affiche avec comme contenue le component InfoModal */}
+      {isModalOpen && isMovieData &&(
+        <Modal
+          isOpen={isModalOpen}
+          onRequestClose={handleCloseModal}
+        >
+          {/* Props setIsModalOpen vas servir à savoir si la modal est ouverte ou pas depuis le composant InfoModal*/}
+          <InfoModal data={isMovieData} setIsModalOpen={setIsModalOpen}/>
+        </Modal>
+      )}
     </main>
   );
 }
