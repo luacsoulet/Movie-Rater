ea47e4ecffac29b9f9750d072c388b00f095d215
lucasoulet <lucasmagalhaes45200@gmail.com>
2024-08-04 01:41:37 +0200

    Movies Page create
    Créationde la page contenant tous les films ajoutés à la platforme et utilisant les composants modal de react-modal mais aussi du composant Card, Modal et Image pour afficher les films et leurs modales lors d'un clique. Pour savoir quel films sera choisis un use State contenant l'id du film pour en récuperer les données.

--- movie-rater/app/(pages)/movies/page.jsx
+++ movie-rater/app/(pages)/movies/page.jsx
@@ -1,6 +1,72 @@
+'use client'
+import Modal from 'react-modal';
+import { useState } from "react";
+import InfoModal from "@/app/components/ui/modal";
+import Card from "@/app/components/ui/card";
+import MoviesData from '@/public/data.json'
+
+Modal.setAppElement('#app');
+
 function Movies(){
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    
+    // UseState pour la récupération des datas du film selectionné
+    const [isMovieData, setIsMovieData] = useState([]);
+    
+    // Sélection des données des films dans le fichier .json de test
+    const data = MoviesData.films;
+
+    const handleMovieSelect = (id) => {
+
+        // Cherche les données suivant l'affiche où l'on a cliqué pour avoir plus d'infos.
+        const movieData = data.find(el => el.id === id);
+    
+        // Mettre les données du film dans le useState 
+        setIsMovieData(movieData);
+    
+        // Définir l'ouverture de la modal sur true pour l'afficher
+        setIsModalOpen(true);
+    };
+    
+    // callback function pour définir la fermeture de la modal lors d'un clique en dehors de la modal
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setIsMovieData(null);
+    };
+
     return(
-        <div>Page Movies</div>
+        <main id='app'>
+            <div className='flex flex-col mb-[250px] w-full content-center items-center gap-10 text-white'>
+                <div className='text-center mt-10 text-xl font-medium'>MOVIES</div>
+                <div className='flex justify-center max-w-custom w-full flex-wrap gap-4'>
+                    {
+                        data.map((el) => (
+                            <Card
+                                key={el.id}
+                                id={el.id}
+                                title={el.title}
+                                year={el.year}
+                                averageRating={el.averageRating}
+                                poster={el.poster}
+                                onMovieSelect={handleMovieSelect}
+                            />
+                        ))
+                    }
+                </div>
+            </div>
+            {/* si le useState isModalOpen = true alors la modal s'affiche avec comme contenue le component InfoModal */}
+            {isModalOpen && isMovieData &&(
+                <Modal
+                    isOpen={isModalOpen}
+                    onRequestClose={handleCloseModal}
+                    className="mt-[140px] p-4 shadow-lg mx-auto my-8 w-full max-w-[1240px] rounded-lg bg-[#131313] bg-opacity-80 backdrop-blur-sm text-white" // Ajoutez vos classes Tailwind ici
+                    overlayClassName="fixed inset-0 bg-black bg-opacity-20"
+                >
+                {/* Props setIsModalOpen vas servir à savoir si la modal est ouverte ou pas depuis le composant InfoModal*/}
+                    <InfoModal data={isMovieData} setIsModalOpen={setIsModalOpen}/>
+                </Modal>
+            )}
+        </main>
     );
 }
 
