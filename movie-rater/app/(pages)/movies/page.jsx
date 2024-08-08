e2ed43c360f87dbd786d180aefb15c80d032ccb2
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-08-08 14:54:44 +0200

    Movies page gap update

--- movie-rater/app/(pages)/movies/page.jsx
+++ movie-rater/app/(pages)/movies/page.jsx
@@ -35,10 +35,10 @@ function Movies(){
     };
 
     return(
-        <main>
+        <main id='appq'>
             <div className='flex flex-col mb-[250px] w-full content-center items-center gap-10 text-white'>
                 <div className='text-center mt-10 text-xl font-medium'>MOVIES</div>
-                <div className='flex justify-center max-w-custom w-full flex-wrap gap-4'>
+                <div className='flex justify-center max-w-custom w-full flex-wrap gap-8 gap-y-8'>
                     {
                         data.map((el) => (
                             <Card
