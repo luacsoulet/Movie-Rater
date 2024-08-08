171802abd6c3c1fb118e62a26a523a2670f89b28
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-08-08 20:15:33 +0200

    Header animation & code update
    Remise en forme du code pour générer les éléments de navigation sur une itération d'un array d'objets. Ainsi que l'ajout d'animation lors du hover et du clique.

--- movie-rater/app/components/ui/header.jsx
+++ movie-rater/app/components/ui/header.jsx
@@ -1,12 +1,37 @@
+'use client'
+import { useState } from "react"
 import Link from "next/link"
 
 function Header(){
+
+    const [isSlected, setIsSelected] = useState('/');
+    
+    const navItems = [
+        { name: 'Home', path: '/' },
+        { name: 'Movies', path: '/movies' },
+        { name: 'Add Movies', path: '/addMovies' },
+        { name: 'Account', path: '/account' },
+    ];
+
+    const handleLinkClick = (path) => {
+        setIsSelected(path)
+    }
+
     return(
         <ul className="flex items-center gap-8 text-white max-w-custom text-lg">
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/movies">Movies</Link></li>
-            <li><Link href="/addMovies">Add Movies</Link></li>
-            <li><Link href="/account">Account</Link></li>
+            {navItems.map((item) => (
+                <li key={item.path}>
+                    <Link 
+                        href={item.path} 
+                        onClick={() => handleLinkClick(item.path)}
+                        className={`
+                        ${ isSlected === item.path ? 'text-xl font-extrabold' : ''}
+                        hover:text-xl hover:font-extrabold transition-all duration-300 ease-in-out`}
+                    >
+                        {item.name}
+                    </Link>
+                </li>
+            ))}
         </ul>
     )
 }
