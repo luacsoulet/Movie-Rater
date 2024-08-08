3aa6f3d7386e9c9e1b62176d7d1efe12bd8c79be
luacsoulet <lucasmagalhaes45200@gmail.com>
2024-08-08 20:14:13 +0200

    Card Animation update
    Ajout des animations au hover de la card pour l'agrandissement de l'image du film. Mais aussi agrandissement toujours avec scale du bouton plus d'informations lors du hover et changement de couleur lors du hover du bouton pour enregistrer les films.

--- movie-rater/app/components/ui/card.jsx
+++ movie-rater/app/components/ui/card.jsx
@@ -40,9 +40,9 @@ function Card({id ,title, year, averageRating, poster, onMovieSelect}){
     }
 
     return(
-        <div className="h-[350px] w-[250px] relative content-end">
+        <div className="h-[350px] w-[250px] group relative content-end rounded-lg overflow-hidden">
             <Image
-                className="rounded-lg"
+                className="rounded-lg group-hover:scale-125 transition ease-in-out duration-500"
                 src={poster}
                 alt={`${title}-image`}
                 label="Image"
@@ -51,7 +51,7 @@ function Card({id ,title, year, averageRating, poster, onMovieSelect}){
                 style={{ objectFit: 'cover' }}
             />
             <div className="flex flex-col content-end p-2.5 gap-1">
-            <button className="absolute top-2 right-2 flex justify-center items-center w-8 h-8 text-xl rounded-3xl bg-[#131313] bg-opacity-50 backdrop-blur-sm"><FontAwesomeIcon className="text-center text-base" icon={faBookmark} /></button>
+            <button className="absolute top-2 right-2 flex justify-center items-center w-8 h-8 text-xl rounded-3xl bg-[#131313] bg-opacity-50 backdrop-blur-sm hover:text-red-500 transition ease-in-out duration-200"><FontAwesomeIcon className="text-center text-base" icon={faBookmark} /></button>
                 <p className="w-fit h-fit px-2 py-1 rounded-lg bg-[#131313] bg-opacity-50 backdrop-blur-sm" >{title}</p>
                 <p className="w-fit h-fit px-2 py-1 rounded-lg bg-[#131313] bg-opacity-50 backdrop-blur-sm">{year}</p>
                 <div className="flex justify-between items-center">
@@ -60,7 +60,7 @@ function Card({id ,title, year, averageRating, poster, onMovieSelect}){
                             {renderPopcornIcons(5)}
                         </div>
                     </div>
-                    <button className="flex justify-center items-center w-8 h-8 text-xl rounded-3xl bg-[#131313] bg-opacity-50 backdrop-blur-sm" onClick={MovieSelectId} aria-label="Afficher plus d'informations sur le film lors du clique"><FontAwesomeIcon icon={faPlus} /></button>
+                    <button className="flex justify-center items-center w-8 h-8 text-xl rounded-3xl bg-[#131313] bg-opacity-50 backdrop-blur-sm hover:scale-125 transition ease-in-out duration-200" onClick={MovieSelectId} aria-label="Afficher plus d'informations sur le film lors du clique"><FontAwesomeIcon icon={faPlus} /></button>
                 </div>
             </div>
         </div>
