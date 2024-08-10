dd862e9a08b769101c7b57ea1724e3cb13a204c5
Lucas Magalhaes <119440425+luacsoulet@users.noreply.github.com>
2024-08-10 18:20:49 +0200

    InfoModal Trailer Update
    Importation de la librairie react-player permettant de manière simple d'affciher en embed une vidéo youtube. Création de useState pour gerer l'affichage et le masquage du trailer via le bouton watch trailer et une flèche retour qui au onClique renvoie une foncion callback pour changer l'état. Puis sur une condition ternaire affichage ou non du trailer ou des infos du film.

--- movie-rater/app/components/ui/infoModal.jsx
+++ movie-rater/app/components/ui/infoModal.jsx
@@ -1,19 +1,32 @@
 'use client'
 import { useState } from 'react';
 import Image from 'next/image';
+import ReactPlayer from 'react-player/lazy';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import popcornImage from '@/public/images/popcorn-rating.svg'
 
 // Props data pour les donnés du film et setIsModalOpen pour savoir si la modal est ouverte depuis le composant
 function InfoModal({data, setIsModalOpen}){
     
+    const [isTrailerShow, setIsTrailerShow ] = useState(false)
     const [indexRating, setIndexRating] = useState(null);
     const [hoveredIndex, setHoveredIndex] = useState(null);
 
     // callback function pour la fermeture de la modale.
     const closeModal = () => {
         setIsModalOpen(false);
+        setIsTrailerShow(false)
+    }
+
+    // callback function pour afficher le trailer.
+    const handleShowTrailer = () => {
+        setIsTrailerShow(true)
+    }
+
+    // callback function pour cacher le trailer.
+    const handleHideTrailer = () => {
+        setIsTrailerShow(false)
     }
 
     // callback function pour le hover d'un icône rating.
@@ -81,41 +94,54 @@ function InfoModal({data, setIsModalOpen}){
         return icons;
     }
 
+    
     return (
         <div className='flex items-center gap-8 min-h-[600px] h-fit'>
-            <div className='relative w-[333px] h-[500px]'>
-                <Image
-                    className='rounded-xl object-cover'
-                    src={data.poster}
-                    alt={data.title}
-                    fill
-                    sizes="(max-width: 768px) 100vw"
+            {isTrailerShow ? (
+                <div className='flex flex-col rounded-xl w-full items-center gap-3'>
+                <ReactPlayer
+                    url={data.trailerUrl} controls={true} height='600px' width='90%'
                 />
+                <button className='absolute top-4 left-4' aria-label="Retourner à la fenêtre d'informations du film" onClick={handleHideTrailer}><FontAwesomeIcon icon={faArrowLeft} className='text-3xl' /></button>
+                <button className="absolute top-4 right-4" onClick={closeModal} aria-label="Fermer la fenêtre d'informations du film"><FontAwesomeIcon icon={faXmark} className='text-3xl'/></button>
             </div>
-            <div className='flex flex-col gap-[8px] w-[720px] text-2xl'>
-                <h1 className='text-3xl'>{data.title}</h1>
-                <p>{data.duration} min</p>
-                <p>{data.year}</p>
-                <div className='flex gap-[8px]'>
-                    {data.genre.map((el, index) => (
-                        <p key={index} className='px-[10px] py-[3px] rounded-2xl bg-red-800 text-xl hover:bg-opacity-70 transition ease-in-out duration-300'>{el}</p>
-                    ))}
-                </div>
-                <p>{data.director}</p>
-                <div className='flex gap-2'>
-                    {data.actors.map((el, index) => (
-                        <p key={index} className='px-[10px] py-[3px] rounded-2xl bg-gray-500 text-xl hover:bg-gray-600 transition ease-in-out duration-300'>{el}</p>
-                    ))}
-                </div>
-                <p className='text-xl'>{data.resume}</p>
-                <div className='flex items-center justify-between'>
-                    <div className="flex items-center">
-                        {renderPopcornIcons(5)}
+            ) : (
+                <>
+                    <div className='relative w-[333px] h-[500px]'>
+                        <Image
+                            className='rounded-xl object-cover'
+                            src={data.poster}
+                            alt={data.title}
+                            fill
+                            sizes="(max-width: 768px) 100vw"
+                        />
                     </div>
-                    <button className='h-10 px-3 rounded-3xl bg-red-800 text-xl hover:bg-opacity-70 transition ease-in-out duration-300'>Watch Trailer</button>
-                </div>
-            </div>
-            <button className="absolute top-4 right-4" onClick={closeModal} aria-label="Fermer la fenêtre d'informations du film"><FontAwesomeIcon icon={faXmark} className='text-3xl'/></button>
+                    <div className='flex flex-col gap-[8px] w-[720px] text-2xl'>
+                        <h1 className='text-3xl'>{data.title}</h1>
+                        <p>{data.duration} min</p>
+                        <p>{data.year}</p>
+                        <div className='flex gap-[8px]'>
+                            {data.genre.map((el, index) => (
+                                <p key={index} className='px-[10px] py-[3px] rounded-2xl bg-red-800 text-xl hover:bg-opacity-70 transition ease-in-out duration-300'>{el}</p>
+                            ))}
+                        </div>
+                        <p>{data.director}</p>
+                        <div className='flex gap-2'>
+                            {data.actors.map((el, index) => (
+                                <p key={index} className='px-[10px] py-[3px] rounded-2xl bg-gray-500 text-xl hover:bg-gray-600 transition ease-in-out duration-300'>{el}</p>
+                            ))}
+                        </div>
+                        <p className='text-xl'>{data.resume}</p>
+                        <div className='flex items-center justify-between'>
+                            <div className="flex items-center">
+                                {renderPopcornIcons(5)}
+                            </div>
+                            <button className='h-10 px-3 rounded-3xl bg-red-800 text-xl hover:bg-opacity-70 transition ease-in-out duration-300' onClick={handleShowTrailer}>Watch Trailer</button>
+                        </div>
+                    </div>
+                    <button className="absolute top-4 right-4" onClick={closeModal} aria-label="Fermer la fenêtre d'informations du film"><FontAwesomeIcon icon={faXmark} className='text-3xl'/></button>
+                </>
+            )}
         </div>
     )
 }
